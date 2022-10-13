c1dd8392f250b8128aac5459a1c95afe22bf8c48
Ruan Finamor <75641084+RuanF7@users.noreply.github.com>
2022-10-13 17:17:59 -0300

    Adicionando Exercício03
    Escreva um algoritmo para determinar o consumo médio de um automóvel 
    sendo fornecida a distância total percorrida pelo automóvel e o 
    total de combustível gasto

---
+++ Exercicio03.java
@@ -0,0 +1,27 @@
+/*Escreva um algoritmo para determinar o consumo médio de um automóvel 
+sendo fornecida a distância total percorrida pelo automóvel e o 
+total de combustível gasto */ 
+package com.mycompany.exercicio03;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        
+        float consumoMedio, distanciaTotal, totalCombustivel;
+      
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite a distância total percorrida: ");
+        distanciaTotal = leitor.nextInt();
+        System.out.println("Agora digite o total de combústivel gasto: ");
+        totalCombustivel = leitor.nextInt();
+        
+        consumoMedio = distanciaTotal /  totalCombustivel ;
+        
+        System.out.println("O consumo médio de combustível do automóvel é de: " 
+                + consumoMedio + "KM/L" );
+    }
+}
